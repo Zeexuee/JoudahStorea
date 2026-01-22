6a9da2a764c365521a9026cf122941986f71adab
Choirul Fahri <ikangapi121@gmail.com>
2026-01-23 04:07:13 +0700

    fix

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -6,7 +6,7 @@
         <!-- HERO SECTION -->
         <div class="relative w-full h-[40vh] min-h-[400px] flex items-center justify-center overflow-hidden bg-gray-100">
             <!-- Background Image -->
-            <img src="{{ asset($category['banner']) }}" alt="{{ $category['title'] }}" class="absolute inset-0 w-full h-full object-cover">
+            <img src="{{ asset('storage/' . $category['hero_image']) }}" alt="{{ $category['title'] }}" class="absolute inset-0 w-full h-full object-cover">
             <div class="absolute inset-0 bg-black/30"></div>
 
             <!-- Content -->
